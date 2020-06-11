9e484c06aeab6b42a2a09cea3f38cd201e67cfb8
HansKu7323 <boy7323@gmail.com>
2020-06-11 16:11:08 +0900

    commit4

--- js/main.js
+++ js/main.js
@@ -4,11 +4,20 @@
   var vm = new Vue({
     el:'#app',
     data:{
+      newItem:'',
       todos:[
         'task1',
         'task2',
         'task3'
       ]
+    },
+    methods:{
+      addItem:function(){
+        // e.preventDefault();ページが遷移無効化する
+        this.todos.push(this.newItem);
+        this.newItem=''
+      }
     }
-  })
+  });
+
 })();
